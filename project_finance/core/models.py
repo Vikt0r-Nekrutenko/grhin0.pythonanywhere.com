a349099ca37074d594d76b89f9ad325e20e934c0
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-03-29 18:56:29 +0300

    007

--- project_finance/core/models.py
+++ project_finance/core/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 
 class CategoryType(models.Model):
-    type = models.CharField(primary_key=True, null=False)
+    type = models.CharField(max_length=255, primary_key=True, null=False)
 
 
 class Category(models.Model):
-    name = models.CharField(primary_key=True)
+    name = models.CharField(max_length=255, primary_key=True)
     type = models.ForeignKey('CategoryType', on_delete=models.PROTECT, null=False)
 
 
 class Deposit(models.Model):
-    name = models.CharField(primary_key=True)
+    name = models.CharField(max_length=255, primary_key=True)
     balance = models.IntegerField()
 
 
