2845daecca55a7ae5d160129ddfe9b335d23f906
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-03-30 00:11:13 +0300

    018

--- project_finance/project_finance/settings.py
+++ project_finance/project_finance/settings.py
@@ -34,6 +34,10 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
 
 INSTALLED_APPS = [
