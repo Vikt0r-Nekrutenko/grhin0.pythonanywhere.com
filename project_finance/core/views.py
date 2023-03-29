e02688af6969f331a5b6f289c3218f6213c7009e
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-03-29 20:28:46 +0300

    012

--- project_finance/core/views.py
+++ project_finance/core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .models import Deposit
+from .serializers import DepositSerializer
 
 
 class DepositAPIView(generics.ListAPIView):
