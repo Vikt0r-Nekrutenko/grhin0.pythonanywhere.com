33ebb5dad7d1d774e3b8c8c5b4f96b02fd6c0e84
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-03-29 20:18:37 +0300

    010

--- project_finance/core/views.py
+++ project_finance/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Deposit
 
-# Create your views here.
+
+class DepositAPIView(generics.ListAPIView):
+    queryset = Deposit.objects.all()
+    serializer_class = DepositSerializer
