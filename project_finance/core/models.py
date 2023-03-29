4a5d64bd2b48c68fa495c4f321199167af5efc19
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-03-29 18:52:52 +0300

    003

--- project_finance/core/models.py
+++ project_finance/core/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class CategoryType(models.Model):
     type = models.CharField(primary_key=True, null=False)
+
+
+class Category(models.Model):
+    name = models.CharField(primary_key=True)
+    type = models.ForeignKey('CategoryType', on_delete=models.PROTECT, null=False)
