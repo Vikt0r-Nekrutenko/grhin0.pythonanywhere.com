9de1e1cb773c72af6ffd831ec9be3b8521f5b8c3
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-03-29 19:05:02 +0300

    008

--- project_finance/core/admin.py
+++ project_finance/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Deposit, Category, Operation, CategoryType
 
-# Register your models here.
+
+admin.site.register((Deposit, Category, Operation, CategoryType))
