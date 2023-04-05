086e9e133288d7fd77883d27bdd1827eedd85e33
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-06 01:49:42 +0300

    045

--- project_finance/project_finance/wsgi.py
+++ project_finance/project_finance/wsgi.py
@@ -8,10 +8,23 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+import django
 
+import django.core.handlers.wsgi
 from django.core.wsgi import get_wsgi_application
+
+path = '/home/grhin0/grhin0.pythonanywhere.com/project_finance/'
+
+if path not in sys.path:
+    sys.path.append(path)
+
+os.chdir(path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_finance.settings')
 
-application = get_wsgi_application()
-app = application
+# application = get_wsgi_application()
+
+django.setup()
+
+application = django.core.handlers.wsgi.WSGIHandler()
