7fe4b8237a8e6d272a0d8bfbb2be998dafd071e0
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-06 15:00:51 +0000

    056

--- project_finance/project_finance/settings.py
+++ project_finance/project_finance/settings.py
@@ -36,8 +36,8 @@ ALLOWED_HOSTS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated',
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.AllowAny',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
