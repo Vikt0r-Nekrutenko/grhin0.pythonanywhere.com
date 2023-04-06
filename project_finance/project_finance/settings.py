614eea24e23f3f58ed2321f688263d2089af865b
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-06 14:49:05 +0000

    055

--- project_finance/project_finance/settings.py
+++ project_finance/project_finance/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -139,9 +139,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = Path(BASE_DIR / 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -151,6 +151,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path(BASE_DIR / 'media')
 
+SECURE_SSL_REDIRECT = True
+
 CSRF_COOKIE_SECURE = True
+
+CSRF_TRUSTED_ORIGINS=[
+    'http://grhin0.pythonanywhere.com',
+]
 
 SESSION_COOKIE_SECURE = True
