4400b0f379b5b0f677ec7dead56f6a95ea25d1cf
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-04-08 00:32:20 +0300

    065

--- project_finance/project_finance/settings.py
+++ project_finance/project_finance/settings.py
@@ -145,16 +145,18 @@ STATIC_URL = '/static/'
 STATIC_ROOT = Path(BASE_DIR / 'static')
 
 STATICFILES_DIRS = [
-   Path(BASE_DIR, 'static/styles')
+    Path(BASE_DIR, 'static/styles'),
+    Path(BASE_DIR, 'static/pics')
 ]
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = Path(BASE_DIR / 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = Path(BASE_DIR / 'media')
 
 SECURE_SSL_REDIRECT = False
 
