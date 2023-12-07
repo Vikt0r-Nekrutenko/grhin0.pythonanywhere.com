7b59e5f902fd1ad0a83435ca29c081551151e7bf
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-12-07 19:25:03 +0200

    071

--- grhin0/grhin0/urls.py
+++ grhin0/grhin0/urls.py
@@ -1,4 +1,4 @@
-"""project_finance URL Configuration
+"""grhin0 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- grhin0/grhin0/wsgi.py
+++ grhin0/grhin0/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for project_finance project.
+WSGI config for grhin0 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_finance.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'grhin0.settings')
 
 application = get_wsgi_application()
