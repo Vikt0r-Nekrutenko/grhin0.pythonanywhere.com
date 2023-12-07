c80855d8326d0293866ca592d3334972eae2d729
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-12-07 19:50:42 +0200

    076

--- grhin0/project_finance/views.py
+++ grhin0/project_finance/views.py
@@ -41,14 +41,14 @@ def main_page_view(request):
     operations_count = Operation.objects.count()
 
     try:
-        str_date =str(Operation.objects.first().date)
+        str_date = str(Operation.objects.first().date)
         date = datetime.strptime(str_date, '%Y-%m-%d').date()
         month_count = int((datetime.now().date() - date).days)
     except:
         month_count = 0
 
     return render(request, 'index.html', {
-                                            'objects_count': objects_count,
-                                            'operations_count': operations_count,
-                                            'month_count': month_count,
-                                        })
+        'objects_count': objects_count,
+        'operations_count': operations_count,
+        'month_count': month_count,
+    })
