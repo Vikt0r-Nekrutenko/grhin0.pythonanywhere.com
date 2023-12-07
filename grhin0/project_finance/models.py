6024ee01816fd6235604694d825ef24aa0300838
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-12-07 21:51:51 +0200

    083

--- grhin0/project_finance/models.py
+++ grhin0/project_finance/models.py
@@ -19,6 +19,7 @@ class Category(models.Model):
 class Deposit(models.Model):
     name = models.CharField(max_length=255, primary_key=True)
     balance = models.IntegerField()
+    version = models.IntegerField(default=1)
 
     def __str__(self):
         return f'{self.name}: [{self.balance}]'
