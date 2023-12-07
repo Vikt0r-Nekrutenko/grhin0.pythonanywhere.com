5f7eec0a5904170fbcd7b62a3731bc52b897e384
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2023-12-07 19:45:41 +0200

    075

--- grhin0/grhin0/settings.py
+++ grhin0/grhin0/settings.py
@@ -153,10 +153,6 @@ STATICFILES_DIRS = [
     Path(BASE_DIR, 'static/pics')
 ]
 
-#MEDIA_URL = '/media/'
-
-#MEDIA_ROOT = Path(BASE_DIR / 'media')
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
